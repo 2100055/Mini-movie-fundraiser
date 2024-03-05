dac8d131bf5ade42ad474e225285f5c49b12a701
2100055 <162372360+2100055@users.noreply.github.com>
2024-03-06 09:47:38 +1300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 
 # Main routine goes here
-want_instructions = input('Do you want to see the instructions?')
+want_instructions = input('Do you want to see the instructions?').lower()
 
 if want_instructions == 'yes':
     print('instructions go here')
