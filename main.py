ceb4488e25f2a8bb5b75061ebb6c5276136570e8
2100055 <162372360+2100055@users.noreply.github.com>
2024-03-06 09:13:52 +1300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# Functions go here
+
+
+# Main routine goes here
+want_instructions = input('Do you want to see the instructions?')
+
+if want_instructions == 'yes':
+    print('instructions go here')
+
+elif want_instructions == 'no':
+    pass
+
+else:
+    print('Please answer Yes / No')
